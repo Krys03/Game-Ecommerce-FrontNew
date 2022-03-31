d3968e52ff26a664a224d13190687f3bbe9b57ca
Christophe Lopez <christophelopez@Christophes-MacBook-Pro.local>
2022-04-01 01:37:41 +0200

    first commit

---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+export const BASE_PATH = "https://game-ecommerce-backend.herokuapp.com";
+export const TOKEN = "token";
+export const CART = "cart";
+export const STRIPE_TOKEN = "pk_test_51Kj29tIs20Xvy5SOq9RuSIUMF2oW1DO8a52eQrBRabADI4AeZn4XLyWn0oKV3R1LKBnLENSEcJ5bXi7K1CJTvLyZ00V1ydnTWT"; 
